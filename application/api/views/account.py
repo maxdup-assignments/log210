21e1d2198994513996ecbd69055d136cc3eaa5ae
maxdup <mdupuis@hotmail.ca>
2015-01-22 15:20:56 -0500

    refactored profile editing and added api authentication

--- application/api/views/account.py
+++ application/api/views/account.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, render_to_response
-from django.http import HttpResponse
-
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect, HttpResponse
-from api.models import UserProfile
+from django.http import HttpResponseRedirect, HttpResponse,  HttpResponseForbidden
+
 from django.contrib.auth.models import User
+from api.models import UserProfile
 
 from api.serializers import ProfileSerializer, UserSerializer
 from rest_framework.renderers import JSONRenderer
@@ -20,7 +19,9 @@ def get_current_profile(request):
     return HttpResponse(JSONRenderer().render(profile.data))
 
 def get_profiles(request):
-    #todo check auth
+    if not request.user.is_superuser:
+        return HttpResponseForbidden()
+
     users = UserProfile.objects.all()
     profiles = {'users':[]}
     for user in users:
@@ -29,26 +30,26 @@ def get_profiles(request):
     return HttpResponse(JSONRenderer().render(profiles))
 
 def edit_profile(request):
-    #todo check auth
+
     if request.method == 'POST':
         userinfo = json.loads(request.body)
+        if request.user.pk != userinfo['user']['pk']:
+            if not request.user.is_superuser:
+                return HttpResponseForbidden()
+
+        del userinfo['backup']
 
         user = User.objects.get(pk=userinfo['user']['pk'])
-        user.first_name = userinfo['user']['first_name']
-        user.last_name = userinfo['user']['last_name']
-        user.username = userinfo['user']['email']
-        user.email = userinfo['user']['email']
-        user.is_superuser = userinfo['user']['is_superuser']
-        user.is_staff = userinfo['user']['is_staff']
+        #magic that updates user fields from json dict
+        user.__dict__.update(**userinfo['user'])
         user.save()
+        del userinfo['user']
 
         profile = UserProfile.objects.get(pk=userinfo['pk'])
-        profile.adresse = userinfo['adresse']
-        profile.telephone = userinfo['telephone']
-        profile.date_naissance = userinfo['date_naissance']
+        profile.__dict__.update(**userinfo)
         profile.save()
 
-    return HttpResponse(json.dumps(userinfo))
+    return HttpResponse(json.dumps({'success': True}))
 
 
 def register(request):
@@ -56,7 +57,6 @@ def register(request):
 
     if request.method == 'POST':
         userinfo = json.loads(request.body)
-        print userinfo
         user = User.objects.create_user(username=userinfo['email'],
                                         first_name=userinfo['first_name'],
                                         last_name=userinfo['last_name'],
