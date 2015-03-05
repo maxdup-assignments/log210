8c17156684c5827055c517ca40c1d20835f95ac3
Philippe <phil_2121@msn.com>
2015-03-05 18:26:53 -0500

    Angular-gettext integration / AngularJS 1.3

---
+++ application/resto/static/js/translations.js
@@ -0,0 +1,5 @@
+angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
+/* jshint -W100 */
+    gettextCatalog.setStrings('ja', {"Address:":"アドレッス","Birthday:":"誕生日","Cancel":"キャンセル","Edit":"エテェット","Email:":"Eメール:","First Name:":"名前：","Hello {{profile.user.first_name}}":"こんにちは {{profile.user.first_name}}","Last Name:":"姓:","Meet your profile!":"プロフィルを合います！","Password:":"パスワード:","Save":"セーブ","Telephone:":"でんわ：","add":"着ける"});
+/* jshint +W100 */
+}]);
