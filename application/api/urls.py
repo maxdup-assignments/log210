cc9a5edcd1f0a885a84d2832ee4a9b225b824601
maxdup <mdupuis@hotmail.ca>
2015-03-19 15:42:09 -0400

    removed unused view

--- application/api/urls.py
+++ application/api/urls.py
@@ -1,26 +1,29 @@
 from django.conf.urls import patterns, url
-from api.views import views
+
+from api.views.account import *
+from api.views.restaurant import *
+from api.views.commande import *
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^register', views.register, name='register'),
-    url(r'^login', views.user_login, name='login'),
-    url(r'^logout', views.user_logout, name='logout'),
-    url(r'^all_staff', views.get_staff, name='get_staff'),
-    url(r'^profile', views.get_current_profile, name='get_current_profile'),
-    url(r'^all_profiles', views.get_profiles, name='get_profiles'),
-    url(r'^edit_profile', views.edit_profile, name='edit_profile'),
-    url(r'^delete_profile', views.delete_profile, name='delete_profile'),
-    url(r'^populateuser', views.populateUser, name='populateUser'),
-    url(r'^create_resto', views.create_resto, name='create_resto'),
-    url(r'^delete_resto', views.delete_resto, name='delete_resto'),
-    url(r'^edit_resto', views.edit_resto, name='edit_resto'),
-    url(r'^all_resto', views.all_resto, name='all_resto'),
-    url(r'^assigned_resto', views.assigned_resto, name='assigned_resto'),
-    url(r'^populateresto', views.populate_resto, name='populateresto'),
-    url(r'^create_commande', views.create_commande, name='create_commande'),
-    url(r'^get_commande', views.get_commande, name='get_commande'),
-    url(r'^resto_commande', views.resto_commande, name='resto_commande'),
-    url(r'^update_commande', views.update_commande_status, name='update_commande_status'),
-    url(r'^populatecommande', views.populate_commande, name='populatecommandes'),
+    url(r'^register', register, name='register'),
+    url(r'^login', user_login, name='login'),
+    url(r'^logout', user_logout, name='logout'),
+    url(r'^all_staff', get_staff, name='get_staff'),
+    url(r'^profile', get_current_profile, name='get_current_profile'),
+    url(r'^all_profiles', get_profiles, name='get_profiles'),
+    url(r'^edit_profile', edit_profile, name='edit_profile'),
+    url(r'^delete_profile', delete_profile, name='delete_profile'),
+    url(r'^populateuser', populateUser, name='populateUser'),
+
+    url(r'^create_resto', create_resto, name='create_resto'),
+    url(r'^delete_resto', delete_resto, name='delete_resto'),
+    url(r'^edit_resto', edit_resto, name='edit_resto'),
+    url(r'^all_resto', all_resto, name='all_resto'),
+    url(r'^assigned_resto', assigned_resto, name='assigned_resto'),
+    url(r'^populateresto', populate_resto, name='populateresto'),
+    url(r'^create_commande', create_commande, name='create_commande'),
+    url(r'^get_commande', get_commande, name='get_commande'),
+    url(r'^resto_commande', resto_commande, name='resto_commande'),
+    url(r'^update_commande', update_commande_status, name='update_commande_status'),
+    url(r'^populatecommande', populate_commande, name='populatecommandes'),
 )
