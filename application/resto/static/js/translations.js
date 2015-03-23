11ae175613b5f00c88d91d41bbaf9457a3f33eea
Philippe <phil_2121@msn.com>
2015-03-23 16:14:44 -0400

    admin_resto.html translated

--- application/resto/static/js/translations.js
+++ application/resto/static/js/translations.js
@@ -1,5 +1,5 @@
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
-    gettextCatalog.setStrings('ja', {"Address:":"アドレッス：","Birthday:":"誕生日：","Cancel":"キャンセル","Edit":"エテェット","Email:":"Eメール：","First Name:":"名前：","Hello {{profile.user.first_name}}":"こんにちは {{profile.user.first_name}}","Last Name:":"姓:","Meet your profile!":"プロフィルを合います！","Password:":"パスワード：","Save":"セーブ","Telephone:":"でんわ：","add":"着ける","hello,":"こんにちは、","login":"ログイン","logout":"ログアウト","register":"レジスター","restaurants":"料理屋","restaurateurs":"料理屋のカントク"});
+    gettextCatalog.setStrings('ja', {"Add":"着ける","Add Menu":"メニューを着ける","Address:":"アドレッス：","Birthday:":"誕生日：","Cancel":"キャンセル","Edit":"エテェット","Email:":"Eメール：","First Name:":"名前：","Hello {{profile.user.first_name}}":"こんにちは {{profile.user.first_name}}","Last Name:":"姓:","Meet your profile!":"プロフィルを合います！","Menu":"メニュー","Name":"名前","Password:":"パスワード：","Remove":"落とす","Save":"セーブ","Telephone:":"でんわ：","add":"着ける","address":"アドレッス","description":"記載","hello,":"こんにちは、","login":"ログイン","logout":"ログアウト","menu...":"メニュー。。。","name":"名前","price":"価格","register":"レジスター","restaurants":"料理屋","restaurateur":"料理屋のカントク","restaurateurs":"料理屋のカントク"});
 /* jshint +W100 */
 }]);
